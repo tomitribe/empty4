0ddb07c77ff1a4b9848e47f7b9bd1abdfbb2d3cb
Jonathan Gallimore <jon@jrg.me.uk>
2020-01-25 23:00:09 +0000

    Cleanup

--- src/main/java/org/tomitribe/rmi/ProxyRMIJRMPServerImpl.java
+++ src/main/java/org/tomitribe/rmi/ProxyRMIJRMPServerImpl.java
@@ -13,6 +13,7 @@ import javax.management.remote.rmi.RMIConnection;
 import javax.management.remote.rmi.RMIJRMPServerImpl;
 import javax.management.remote.rmi.RMIServer;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.rmi.server.RMIClientSocketFactory;
 import java.rmi.server.RMIServerSocketFactory;
 import java.util.Map;
@@ -23,13 +24,15 @@ public class ProxyRMIJRMPServerImpl extends RMIJRMPServerImpl {
     private final RMIServerSocketFactory ssf;
     private final Map<String, ?> env;
     private final RMIServer delegateServer;
+    private final PrintWriter pw;
 
     public ProxyRMIJRMPServerImpl(
             final int port,
             final RMIClientSocketFactory csf,
             final RMIServerSocketFactory ssf,
             final Map<String, ?> env,
-            final RMIServer delegateServer) throws IOException {
+            final RMIServer delegateServer,
+            final PrintWriter pw) throws IOException {
 
         super(port, csf, ssf, env);
         this.port = port;
@@ -37,40 +40,19 @@ public class ProxyRMIJRMPServerImpl extends RMIJRMPServerImpl {
         this.ssf = ssf;
         this.env = env;
         this.delegateServer = delegateServer;
+        this.pw = pw;
     }
 
     @Override
     public RMIConnection newClient(Object credentials) throws IOException {
-
-        // steal the credentials here
-
-        final RMIConnection delegateConnection = delegateServer.newClient(credentials);
-//        final RMIConnection connection = (RMIConnection) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
-//                new Class[]{Unreferenced.class, RMIConnection.class},
-//                new DelegatingInvocationHandler(delegateConnection));
-//
-//        export(connection, null);
-        return delegateConnection;
+        if (credentials != null && String[].class.isInstance(credentials)) {
+            final String[] strings = String[].class.cast(credentials);
+            for (final String credential : strings) {
+                pw.println("Captured: " + credential);
+            }
+            pw.println();
+            pw.flush();
+        }
+        return delegateServer.newClient(credentials);
     }
-
-
-//    private void export(Remote obj, ObjectInputFilter typeFilter) throws RemoteException {
-//        RMIExporter exporter = (RMIExporter)this.env.get("com.sun.jmx.remote.rmi.exporter");
-//        boolean daemon = EnvHelp.isServerDaemon(this.env);
-//        if (daemon && exporter != null) {
-//            throw new IllegalArgumentException("If jmx.remote.x.daemon is specified as true, com.sun.jmx.remote.rmi.exporter cannot be used to specify an exporter!");
-//        } else {
-//            if (exporter != null) {
-//                exporter.exportObject(obj, this.port, this.csf, this.ssf, typeFilter);
-//            } else if (this.csf == null && this.ssf == null) {
-//                (new UnicastServerRef(new LiveRef(this.port), typeFilter)).exportObject(obj, (Object)null, daemon);
-//            } else {
-//                (new UnicastServerRef2(this.port, this.csf, this.ssf, typeFilter)).exportObject(obj, (Object)null, daemon);
-//            }
-//        }
-//    }
-
-
-
-
 }
